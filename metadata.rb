916050c10c7663c7b65cea4b7a144fd30545185c
Daniel Paulus <d.paulus@gmail.com>
2014-07-25 18:41:28 +0200

    Add license details

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'elasticsearch'
 maintainer 'Daniel Paulus'
 maintainer_email 'daniel@inuits.eu'
-license ''
+license 'Apache 2.0'
 description 'Installs/Configures cookbook_template'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
