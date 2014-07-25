69a26f7e16833af1f647e5a7a089e7b08f2ffab4
Daniel Paulus <d.paulus@gmail.com>
2014-07-25 18:36:52 +0200

    Add serverspec intregration tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,7 +1,25 @@
 require 'spec_helper'
 
-describe 'Cookbook template' do
+describe package('elasticsearch') do
+  it { should be_installed  }
+end
 
-  # TODO: add integration test
+describe package('java-1.7.0-openjdk') do
+  it { should be_installed }
+end
 
+describe service('elasticsearch') do
+  it { should be_running }
+end
+
+describe port('9200') do
+  it { should be_listening }
+end
+
+describe port('9300') do
+  it { should be_listening }
+end
+
+describe file('/etc/yum.repos.d/ElasticSearch.repo') do
+  it { should be_file }
 end
